6509d15b6887d6d4a1cfa7b7cc7b82fcd5908a48
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2024-10-13 21:50:45 -0500

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,11 @@
+EMPTY = ' '
+SIMPLE_GAME = 'Simple Game'
+GENERAL_GAME = 'General Game'
+COMPUTER = 'Computer'
+HUMAN = 'Human'
+BAD_MOVE = 'Bad move'
+GOOD_MOVE = 'Good move'
+FULL_BOARD = 'Full Board'
+SCORE = 'Score'
+DRAW = 'Draw'
+NULL = None
